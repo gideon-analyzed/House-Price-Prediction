1d9493105ee7ec47b32198e964bb06c4fb281f69
Paul Gideon Wabwire <98609842+gideon-analyzed@users.noreply.github.com>
2026-01-28 18:09:55 +0000

    Initialize libraries and install required packages
    Added installation and loading of required R packages.

---
+++ libraries.r
@@ -0,0 +1,22 @@
+# Install required packages
+install.packages("ggplot2")
+install.packages("dplyr")
+install.packages("Hmisc")
+install.packages("reshape")
+install.packages("psych")
+install.packages("corrplot")
+install.packages("car")
+install.packages("scales")
+
+# Load required libraries
+library(ggplot2)
+library(dplyr)
+library(Hmisc)
+library(reshape)
+library(psych)
+library(corrplot)
+library(car)
+library(scales)
+
+# Global options
+options(scipen=999) # avoid scientific representation of figures
